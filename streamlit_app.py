b50455f5fa3949559aefea44e37feade5ebd2d1b
Andrew Burton <andreburto@users.noreply.github.com>
2025-09-02 20:01:23 -0500

    Move API request inside ingredient selection loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 # Import python packages
+import requests
 import streamlit as st
+
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -24,16 +26,13 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
 if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
 
